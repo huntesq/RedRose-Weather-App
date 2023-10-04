4212f8ffe006d76717ddf067cc18de873817be39
Masedi Ncamani <masedi.ncamani@gmail.com>
2023-10-04 10:54:15 +0200

    Correctly linked JS file to HTML.

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ function formatDate(timestamp) {
   let hours = date.getHours();
   let minutes = date.getMinutes();
   let day = date.getDay();
+  return `${day} ${hours}:${minutes} `;
 }
 
 function displayTemperature(response) {
@@ -18,12 +19,11 @@ function displayTemperature(response) {
   cityElement.innerHTML = response.data.name;
   descriptionElement.innerHTML = response.data.weather[0].description;
   humidityElement.innerHTML = response.data.main.humidity;
-  windElement.innerHTML = Math.round(response.data.main.wind.speed);
+  windElement.innerHTML = Math.round(response.data.wind.speed);
   dateElement.innerHTML = formatDate(response.data.dt * 1000);
 }
 
-let apiKey = "8ba84ff1f0d264o7e59dft09ea490231";
-let apiUrl =
-  "https://api.openweathermap.org/data/2.5/weather?q=NewYork&appid=`${apiKey}&units=metric`";
+let apiKey = "bd3bb6534458ba51b48c49f5155745b6";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=New York&appid=${apiKey}&units=metric`;
 
 axios.get(apiUrl).then(displayTemperature);
