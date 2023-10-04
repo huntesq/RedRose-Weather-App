e6226462ecfb023be67cc8b5fd84c7634ccd5fae
Masedi Ncamani <masedi.ncamani@gmail.com>
2023-10-04 13:17:05 +0200

    Included JS for date and time.
    Noticed CSS has been affected and HTML is missing a few elements. Will revert back to that after JS is complete.

--- src/index.js
+++ src/index.js
@@ -3,7 +3,25 @@ function formatDate(timestamp) {
   let date = new Date(timestamp);
   let hours = date.getHours();
   let minutes = date.getMinutes();
-  let day = date.getDay();
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
+  let day = days[date.getDay()];
+
+  if (hours < 10) {
+    hours = `0${hours} `;
+  }
+
+  if (minutes < 10) {
+    minutes = `0${minutes} `;
+  }
+
   return `${day} ${hours}:${minutes} `;
 }
 
